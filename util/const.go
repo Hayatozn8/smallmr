d0b9840bf9f8566127bb1c42df3ac27bfe160742
Hayatozn8 <43396666+Hayatozn8@users.noreply.github.com>
2019-11-05 17:25:21 +0800

    update

--- util/const.go
+++ util/const.go
@@ -4,5 +4,8 @@ const (
 	DEFAULT_BUFFER_SIZE       = 64 * 1024
 	CR                        = '\r'
 	LF                        = '\n'
-	BlockSize           int64 = 268435456
+	STORAGE_UNIT_1B = 1
+	STORAGE_UNIT_1KB = 1024
+	STORAGE_UNIT_1MB = 1048576
+	BlockSize           int64 = 33554432 // 32M //268435456
 )
