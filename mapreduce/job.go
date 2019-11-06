c8c8db04631294cacf6ab0580b182d2c2bb42aab
Hayatozn8 <43396666+Hayatozn8@users.noreply.github.com>
2019-11-06 17:38:59 +0800

    Update job.go

--- mapreduce/job.go
+++ mapreduce/job.go
@@ -4,21 +4,73 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"reflect"
 
 	"github.com/Hayatozn8/smallmr/config"
 	"github.com/Hayatozn8/smallmr/split"
 )
 
 type Job struct {
-	inputFormatObj InputFormat
+	inputFormatObj InputFormat //TODO should be type, (delete)
+	inputFormatClass reflect.Type //system should set defalut inputFormatClass when execute Submit()
+	mapperClass 	reflect.Type
+	reducerClass 	reflect.Type
 	inputPaths     []string
 	conf           *config.Configuration
+	fileSplits     []split.InputSplit
 }
 
-func NewJob(conf *config.Configuration) JobContext {
+func NewJob(conf *config.Configuration) *Job {
 	return &Job{
 		conf: conf,
 	}
+}
+
+//TODO
+// interface implement
+func (job *Job) GetNumReduceTasks() int {
+	return 0
+}
+
+//TODO
+// interface implement
+func (job *Job) GetInputFormatClass() (InputFormat, error) {
+	return nil, nil
+}
+
+//TODO
+func (job *Job) SetInputFormatClass(format InputFormat) {
+	job.inputFormatObj = format
+}
+
+//TODO
+// interface implement
+func (job *Job) GetMapperClass() (reflect.Type, error) {
+	return nil, nil
+}
+
+//TODO job or config ????
+func (job *Job) SetMapperClass(nilMapObj Mapper) {
+	t := reflect.TypeOf(nilMapObj)
+	if t.Kind() == reflect.Ptr{
+		t = t.Elem()
+	}
+
+	// TODO:
+	// reflect.New(t).Interface()
+	// check func(x *X) and func(x X) 
+	job.mapperClass = t
+}
+
+//TODO
+// interface implement
+func (job *Job) GetReducerClass() (reflect.Type, error) {
+	return nil, nil
+}
+
+// interface implement
+func (job *Job) GetConfiguration() *config.Configuration {
+	return job.conf
 }
 
 func (job *Job) Submit() error {
@@ -32,13 +84,23 @@ func (job *Job) Submit() error {
 		return err //TODO
 	}
 
+	// TODO:len(split) == 0 ????
+	job.fileSplits = splitList
+
 	// TODO: wait end
 	var wg sync.WaitGroup
-	wg.Add(len(splitList))
-	fmt.Println("split count = ", len(splitList))
-	fmt.Println("all split count = ", splitList)
+	wg.Add(len(job.fileSplits))
+	fmt.Println("split count = ", len(job.fileSplits))
+	fmt.Println("all split count = ", job.fileSplits)
+
+	job.fileSplits = MAX_TASK_COUNT
+
+	NewTaskTracker()
+
+
+
 	// switch to taskContext
-	for i, splitInfo := range splitList {
+	for i, splitInfo := range job.fileSplits {
 		go func(splitInfo split.InputSplit, index int) {
 			defer wg.Done()
 			reader := job.inputFormatObj.CreateRecordReader(splitInfo, job)
@@ -67,43 +129,11 @@ func (job *Job) Submit() error {
 	return nil
 }
 
-func (job *Job) SetInputFormatClass(format InputFormat) {
-	job.inputFormatObj = format
-}
 
-//TODO
-func (job *Job) GetNumReduceTasks() int {
-	return 0
-}
 
-//TODO
-func (job *Job) GetInputFormatClass() (InputFormat, error) {
-	return nil, nil
-}
 
-//TODO
-func (job *Job) GetMapperClass() (Mapper, error) {
-	return nil, nil
-}
 
-//TODO
-func (job *Job) GetReducerClass() (Reduce, error) {
-	return nil, nil
-}
-
-func (job *Job) SetInputPaths(paths ...string) {
-	job.inputPaths = paths
-}
-
-func (job *Job) GetInputPaths() []string {
-	return job.inputPaths
-}
-
-func (job *Job) GetConfiguration() *config.Configuration {
-	return job.conf
-}
-
-//TODO
+//TODO ????????????????????????///
 func (job *Job) NextKeyValue() (bool, error) {
 	return false, nil
 }
