022edf79cc21c44711c12775dc3c3187df3b3d51
Hayatozn8 <43396666+Hayatozn8@users.noreply.github.com>
2019-11-06 17:38:03 +0800

    update

--- config/keyConst.go
+++ config/keyConst.go
@@ -1,9 +1,14 @@
 package config
 
 const (
+	// input path join sep
+	INPUT_PATHS_JOIN_SEP = " "
+
 	// config key
 	SPLIT_MAXSIZE = "SPLIT_MAXSIZE" //int64
 	SPLIT_MINSIZE = "SPLIT_MINSIZE" //int64
 	FILE_DELIMITER = "FILE_DELIMITER" //string
 	MAX_LINE_LENGTH = "MAX_LINE_LENGTH" //string
+	INPUT_PATHS = "inputPaths"
+	MAX_TASK_COUNT = "MAX_TASK_COUNT"
 )
